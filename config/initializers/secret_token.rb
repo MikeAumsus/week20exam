74aec05b02b20e1cf06caef79af05aba8e01f8bd
' <'>
2014-01-22 14:32:15 -0600

    Changed readme.doc
    Changed it and added a joke

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Week20exam::Application.config.secret_key_base = '4e68ba8475649cb884c786a6742c287e942f1135f7f7d69857881276e80c6bc20682014be05336946e3d3f0e0be32d18514e4d7978b64ad738dd20d4752ae652'
